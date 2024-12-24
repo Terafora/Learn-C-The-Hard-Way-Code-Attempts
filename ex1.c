e67f71d08ddf302256ce311599b1f92d97862d1d
Charlotte Stone <charlie.stone649@gmail.com>
2024-12-24 20:47:10 +0000

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+	char name[] = "Charlotte Stone";
+	char language[] = "C";
+	
+	// this is also a comment
+	printf("You are %d miles away.\n", distance);
+	printf("My name is %s.\n", name);
+	printf("This is my latest attempt to learn %s.\n", language);
+	printf("Wish me luck.");
+
+	return 0;
+}
